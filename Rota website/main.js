97c46c094f5cab618296cfdcdfd23146870ac5b0
Rcbmsy0 <115336399+Rcbmsy0@users.noreply.github.com>
2022-10-08 10:57:36 +0530

    Add files via upload

---
+++ Rota website/main.js
@@ -0,0 +1,40 @@
+// navbar change on scroll
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>100)
+})
+
+
+// faqs
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq =>{
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('open');
+        const icon= faq.querySelector('.faq__icon i');
+        if(icon.className==='uil uil-plus'){
+            icon.className="uil uil-minus";
+        }
+        else{
+            icon.className="uil uil-plus"
+        }
+    })
+})
+
+
+//hamburger menu
+const menu=document.querySelector(".nav__menu");
+const menubtn=document.querySelector("#open-menu-btn");
+const closebtn=document.querySelector("#close-menu-btn");
+
+
+menubtn.addEventListener('click',()=>{
+    menu.style.display="flex";
+    closebtn.style.display="inline-block";
+    menubtn.style.display="none"
+})
+const closenav=()=>{
+    menu.style.display="none";
+    closebtn.style.display="none";
+    menubtn.style.display="inline-block"
+}
+closebtn.addEventListener('click',closenav);
